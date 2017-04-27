78103265291ed0cd81d686ef28ce631f97ad29a8
Aykut Kardaş <aykutkrds@gmail.com>
2017-04-27 17:22:40 +0300

    combo kontrol eklendi.
    Doğru çalışıyor.

--- assets/js/control.js
+++ assets/js/control.js
@@ -40,7 +40,10 @@ function GameControl(machine) {
         horizon: [],
         vertical: []
     };
-
+    this.gridsIndex = {
+        horizon: [],
+        vertical: []
+    }
     this.createGrids = function () {
 
         var x = machine.x;
@@ -48,15 +51,19 @@ function GameControl(machine) {
 
         for (var i = 0; i < y; i++) {
             this.grids.horizon[i] = [];
+            this.gridsIndex.horizon[i] = [];
             for (var k = (i * x); k < ((i * x) + x); k++) {
-                this.grids.horizon[i].push(k);
+                this.grids.horizon[i].push(game.virtualBoard[k]);
+                this.gridsIndex.horizon[i].push(k);
             }
         }
 
         for (var i = 0; i < x; i++) {
             this.grids.vertical[i] = [];
+            this.gridsIndex.vertical[i] = [];
             for (var k = i; k < (x * y) + i; k += x) {
-                this.grids.vertical[i].push(k);
+                this.grids.vertical[i].push(game.virtualBoard[k]);
+                this.gridsIndex.vertical[i].push(k);
             }
         }
 
@@ -66,22 +73,22 @@ function GameControl(machine) {
 
         var whichPack = {}
 
-        for (var i = 0; i < this.grids.horizon.length; i++) {
-
-            var result = this.grids.horizon[i].indexOf(index);
+        for (var i = 0; i < this.gridsIndex.horizon.length; i++) {
+            console.log("horizon", whichPack);
+            var result = this.gridsIndex.horizon[i].indexOf(index);
 
             if (result > -1) {
-                whichPack.horizon = this.grids.horizon[i];
+                whichPack.horizon = this.gridsIndex.horizon[i];
             }
 
         }
 
-        for (var i = 0; i < this.grids.vertical.length; i++) {
-
-            var result = this.grids.vertical[i].indexOf(index);
+        for (var i = 0; i < this.gridsIndex.vertical.length; i++) {
+            console.log("vertical", whichPack);
+            var result = this.gridsIndex.vertical[i].indexOf(index);
 
             if (result > -1) {
-                whichPack.vertical = this.grids.vertical[i];
+                whichPack.vertical = this.gridsIndex.vertical[i];
             }
 
         }
@@ -180,6 +187,47 @@ function GameControl(machine) {
 
 
         return false;
+
+    }
+
+    this.boardAnalyze = function () {
+        var a = this.grids.horizon;
+        var horizonDefine = [];
+        for (var i = 0; i < a.length; i++) {
+            for (j = 0; j < a[i].length; j++) {
+
+                if (a[i][j] == a[i][j + 1] && a[i][j] == a[i][j + 2]) {
+                    horizonDefine.push([i, j, j + 1, j + 2]);
+
+                    game.virtualBoard[(i * game.x) + j] = undefined;
+                    game.virtualBoard[(i * game.x) + j + 1] = undefined;
+                    game.virtualBoard[(i * game.x) + j + 2] = undefined;
+                }
+                console.log(a[i][j]);
+
+
+            }
+        }
+
+        var b = this.grids.vertical;
+        var verticalDefine = [];
+        for (var i = 0; i < b.length; i++) {
+            for (j = 0; j < b[i].length; j++) {
+
+                if (b[i][j] == b[i][j + 1] && b[i][j] == b[i][j + 2]) {
+                    verticalDefine.push([i, j, j + 1, j + 2]);
+                    game.virtualBoard[(i * game.y) + j] = undefined;
+                    game.virtualBoard[(i * game.y) + j + 1] = undefined;
+                    game.virtualBoard[(i * game.y) + j + 2] = undefined;
+                }
+                console.log(b[i][j]);
+            }
+        }
+
+        console.log(horizonDefine, verticalDefine);
+
+
+
 
     }
 
